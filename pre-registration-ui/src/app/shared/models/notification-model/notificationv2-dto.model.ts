9565120ff81eb6127a2a67101b9d770ec98bd951
Karthik-TF <karthik.s@technoforte.co.in>
2024-11-06 16:33:05 +0530

    acknowledgement notification

---
+++ pre-registration-ui/src/app/shared/models/notification-model/notificationv2-dto.model.ts
@@ -0,0 +1,15 @@
+export class NotificationDtoModelv2 {
+    constructor(
+      public name: string,
+      public preRegistrationId: string,
+      //malay
+      public appointmentDate: string,
+      public appointmentTime: string,
+      public mobNum: string,
+      public emailID: string,
+      public additionalRecipient: boolean,
+      public isBatch: boolean,
+      public userService: string
+    ) {}
+  }
+  
